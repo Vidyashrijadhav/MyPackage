5b35b56dd46fd08f25fb1539304839c504052802
Vidyashri Jadhav <jadhavvidyashri@gmail.com>
2020-07-08 16:03:31 +0530

    Initial Commit

---
+++ Sources/MyPackage/Service.swift
@@ -0,0 +1,19 @@
+//
+//  Service.swift
+//  SwiftPackageManger
+//
+//  Created by BNT ADMIN on 7/7/20.
+//  Copyright © 2020 Abhishek R. All rights reserved.
+//
+
+import Foundation
+
+// The My Package perform following operation
+
+public struct Service{
+    
+    public static func doSomethingSpecial(){
+        
+        print("Something Special Happend")
+    }
+}
